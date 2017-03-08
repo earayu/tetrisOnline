2f3c3e7bfacaa827552dac493c166e1c2a44ec2d
yuguanghui <yuguanghui@lianfan.com>
2017-03-08 16:36:33 +0800

    调整客户端代码

--- TetrisClient.py
+++ TetrisClient.py
@@ -8,7 +8,7 @@ def draw(board, x,y):
     BSURFACE = board.draw_game_board()
     SURFACE.blit(BSURFACE,(x,y))
 
-
+#TODO 告诉服务器退出，服务器应该也要有心跳测试
 def terminate():
     pygame.quit()
     sys.exit()
@@ -17,7 +17,6 @@ def terminate():
 def request(opr):
     data = '{"game_id":"' + str(cli_player.game_id) + '","player_id":"' + str(cli_player.player_id) + '","opr":"' + opr + '"}'
     return data.encode('utf-8')
-
 
 
 def get_board(sock):
@@ -37,12 +36,46 @@ def get_board(sock):
             svr_player.board.active_shape.x = recv_data["x"]
             svr_player.board.active_shape.y = recv_data["y"]
 
+def send_data(sock, data):
+    sock.send(data)
+    get_board(sock)
+
 # 加载这局游戏的基本数据
 def load_basic_info(sock):
     game_info_json = json.loads(sock.recv(1024).decode('utf-8'))
     game_id = game_info_json["game_id"]
     player_id = int(game_info_json["player_id"])
     return Player(game_id, player_id, sock, Board(16,28)),Player(game_id, player_id, sock, Board(16,28)) #TODO board参数
+
+
+# 每隔4帧数处理一次
+def process_key_event(sock, frames=4):
+    global key_dir,frame_count
+    frame_count += 1
+    for event in pygame.event.get():
+        key_dir = None
+        if event.type == QUIT:
+            terminate()
+        pressed_keys = pygame.key.get_pressed()
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                send_data(sock, request('up'))
+        if pressed_keys[K_LEFT]:
+            key_dir = K_LEFT
+        if pressed_keys[K_RIGHT]:
+            key_dir = K_RIGHT
+        if pressed_keys[K_DOWN]:
+            key_dir = K_DOWN
+
+    # 每4帧
+    if key_dir is not None and frame_count > frames:
+        frame_count = 0
+        if key_dir == K_LEFT:
+            send_data(sock, request('left'))
+        if key_dir == K_RIGHT:
+            send_data(sock, request('right'))
+        if key_dir == K_DOWN:
+            send_data(sock, request('down'))
 
 
 HOST, PORT = "localhost", 9999
@@ -59,46 +92,13 @@ pygame.display.set_caption("俄罗斯方块")
 fpsClock = pygame.time.Clock()
 
 key_dir = None
-frameCount = 0
+frame_count = 0
 
 while True:
     get_board(sock)
-    # 渲染
-    frameCount += 1
-    for event in pygame.event.get():
-        key_dir = None
-        if event.type == QUIT:
-            terminate()
-        pressed_keys = pygame.key.get_pressed()
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
-                sock.sendall(request('up'))
-            if event.key == K_w:
-                svr_player.board.move_piece(K_UP)
-        if pressed_keys[K_LEFT]:
-            key_dir = K_LEFT
-        if pressed_keys[K_a]:
-            key_dir2 = K_a
-        if pressed_keys[K_RIGHT]:
-            key_dir = K_RIGHT
-        if pressed_keys[K_d]:
-            key_dir2 = K_d
-        if pressed_keys[K_DOWN]:
-            key_dir = K_DOWN
-        if pressed_keys[K_s]:
-            key_dir2 = K_s
-    if key_dir in [K_LEFT,K_RIGHT,K_DOWN] and frameCount > 4:
-        frameCount = 0
-        if key_dir == K_LEFT:
-            sock.sendall(request('left'))
-        if key_dir == K_RIGHT:
-            sock.sendall(request('right'))
-        if key_dir == K_DOWN:
-            sock.sendall(request('down'))
-
+    process_key_event(sock)
     draw(cli_player.board,0,0)
     draw(svr_player.board,280,0)
-    # 渲染一帧
     pygame.display.update()
     fpsClock.tick(FPS)
 
